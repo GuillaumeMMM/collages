9a24e0deedb078ae0d70f550434de7a98ca93568
Guillaume M <gmeigniez@manadge.com>
2023-06-17 11:49:02 +0200

    Update cred config

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,23 @@ export async function getServerSideProps({ query }) {
 
   const auth = await google.auth.getClient({
     scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+    credentials: {
+      "type": process.env.GOOGLE_API_TYPE,
+      "project_id": process.env.GOOGLE_API_PROJECT_ID,
+      "private_key_id": process.env.GOOGLE_API_PRIVATE_KEY_ID,
+      "private_key": process.env.GOOGLE_API_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      "client_email": process.env.GOOGLE_API_CLIENT_EMAIL,
+      "client_id": process.env.GOOGLE_API_CLIENT_ID,
+      "auth_uri": process.env.GOOGLE_API_AUTH_URI,
+      "token_uri": process.env.GOOGLE_API_TOKEN_URI,
+      "auth_provider_x509_cert_url": process.env.GOOGLE_API_AUTH_PROVIDER_X509_CERT_URL,
+      "client_x509_cert_url": process.env.GOOGLE_API_CLIENT_X509_CERT_URL,
+      "universe_domain": process.env.GOOGLE_API_UNIVERSE_DOMAIN
+    }
   })
 
   const sheet = google.sheets({ version: 'v4', auth });
-  
+
   const range = `!A01:C10`;
 
   const response = await sheet.spreadsheets.values.get({
@@ -51,7 +64,7 @@ export default function Collages({ collages }) {
       </nav>
     </div>
 
-    
-      <Caroussel images={images}></Caroussel>
+
+    <Caroussel images={images}></Caroussel>
   </div>
 }
